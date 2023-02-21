278d783fdb3bab87e9931cdc77fa023fe6884f55
pskalladasetti <pskalladasetti@XVLT0490>
2023-02-21 10:01:25 +0530

    commit

---
+++ OOPs/src/practise/xtg/com/ArthematiOperations.java
@@ -0,0 +1,32 @@
+package practise.xtg.com;
+
+public class ArthematiOperations {
+
+	public int add(int x, int y) {
+		int z=x+y;
+		System.out.println("sum is: "+ z);
+		return z;
+	}
+
+	public int sub(int x, int y) {
+		int z;
+		if (x > y) {
+			z=x-y;
+			System.out.println("1st num is greater and diff is : "+ z);
+			
+			return z;
+		} else{
+			z=y-x;
+			System.out.println("2nd num is greater and diff is : "+ z);
+			return z;
+		}
+			
+	}
+
+	public int multiply(int x, int y) {
+		int z=x*y;
+		System.out.println("product is: "+ z);
+		
+		return z;
+	}
+}
